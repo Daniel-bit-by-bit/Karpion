b63a735f02d77a2a23ddafa0959bb4139414b4f8
blake046 <blake046@cougars.csusm.edu>
2021-11-01 13:21:09 -0700

    ttt

---
+++ Karpion/Project/js/Messages.js
@@ -0,0 +1,22 @@
+var J = "Jane: Hey are you still interested in our job opening?";
+var K = "Kevin: I'd like to discuss the possibility of hiring you sometime...";
+var L = "Lydia: We'd love to hear back from you...";
+var M = "Marcos: Are you able to meet the skillset i need?";
+var N = "Natalia: Are you able to meet for a face to face interview?";
+var O = "Oliver: I'm not sure we fit each others needs, sorry...";
+var P = "Pearl: We would love to hear back from you in regards to our offer...";
+var Q = "Quincy: Have you considered my offer to work with us on this project?";
+var R = "Robert: I regret to inform you that we already filled all our vacant programming positions...";
+var S = "Stephanie: I'm sorry, we already have a full team at Team Inc....";
+var T = "Tyler: Splendid hearing from you! Let's discuss a contract over lunch?";
+var U = "Ulysses: Would you like to work for us?";
+var V = "Vanessa: Hey, we'd love to have you joion our team here at WebDev Inc.!";
+var W = "Waldo: Here's a bit about us:....";
+var X = "Xavier: I'm looking to hire recently graduated web developers...";
+var Y = "Yvonne: Would you be interested in a developer position here at Hiring Inc.?";
+var Z = "Zoey: My apologies, we're full staffed here at Z World...";
+
+function readMessages(x){
+  document.write(x);
+  return;
+}
