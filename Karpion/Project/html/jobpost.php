57fd3c5a08f7ad992d8c7dcba0e6ee6c81592a68
tomray22 <raythomascruz@gmail.com>
2021-12-08 02:00:39 -0800

    Matching the citrix version
    Adjusting the posting files to match those of the ones on citrix

--- Karpion/Project/html/jobpost.php
+++ Karpion/Project/html/jobpost.php
@@ -6,85 +6,45 @@
     <link rel="stylesheet" type="text/css" href="../css/styles.css" />
     <link rel="stylesheet" href="../css/carousel.css" />
   </head>
+<body>
+<?php
+$servername="localhost";
+$dbUsername="group3";
+$dbPassword="5J7pONKvNY2K";
+$dbName="group3";
+$conn = mysqli_connect($servername,$dbUsername,$dbPassword,$dbName);
 
-  <body>
-    <!-- Header Code Provided by Akhil -->
-    <header>
-      <nav id="globalnav">
-        <!--Logo goes here, brings us back to Landing-->
-        <a href="index.html" id="logo"
-          ><img src="../images/logo.png" alt="Karpion Logo"
-        /></a>
-        <h1>Karpion</h1>
-        <a id="menu"><img src="../images/menu-icon.svg" /></a>
-        <ul>
-          <!--The Other Web Pages Go Into This List-->
-          <li><a href="other.html">Other Page</a></li>
-        </ul>
-      </nav>
-    </header>
-    <script src="../js/jquery-3.6.0.js"></script>
-    <!-- Menu and Globalnav-related Scripts /!-->
-    <script>
-      $(document).ready(function () {
-        // Menu Button Toggle
-        $("#menu").click(function () {
-          $("#globalnav").toggleClass("nav_open");
-        });
+$er = mysqli_select_db($conn,$dbName);
+if (!$er) {
+	print("could not connect to database");
+    exit;
+}
+$SQLstring = "SELECT Company, JobTitle, Description, Eligibility FROM EmployerUsers ORDER BY RAND() LIMIT 3";
 
-        //Remove nav_open class on nav-link click
-        $("#globalnav ul li a").click(function () {
-          $("#globalnav").removeClass("nav_open");
-        });
-      });
-    </script>
-    <!-- End of header code -->
-    <div class="center">
-      <div class="wrapper">
-        <div class="inner">
-          <!-- PHP Script to grab info from database for three random profiles-->  
-            <?php
-            $dbservername="localhost";
-            $dbusername="group3";
-            $dbpass="5J7pONKvNY2K";
-            $dbname="group3";
-            $conn=@mysqli_connect($dbservername,$dbusername,$dbpass,$dbname);
-        
-            // Connection Check
-            if (!$conn) {
-              print("ERROR - Connection failed: could not connect to MySQL");
-              exit;
-            }
-        
-            //Database Select
-            $er = mysqli_select_db($conn,$dbname);
-            if (!$er) {
-              print("ERROR - Connection failed: could not connect to database");
-              exit;
-            }
-        
-            $SQLstring = "SELECT Company, Job_Title, Duties, Qualifications  FROM JOB_POSTING ORDER BY RAND() LIMIT 3";
-        
-            //Execute calls
-            $result = mysqli_query($SQLstring);
-            if(!$result) {
-              print "Error = the query could not be executed";
-              $error = mysqli_error();
-              print "<p>" . $error . "</p>";
-              exit;
-            }
-            do{
-              echo "<div class ="card">";
-              echo "<div class ="content">";
-              echo "<h1>" . $row['Company'] . "</h1>";
-              echo "<h1>" . $row['Job_Title'] . "</h1>";
-              echo "<h3>" . $row['Duties'] . "</h3>";
-              echo "<h3>" . $row['Qualifications'] . "</h3>";
-              echo "</div>"
-              echo "</div>"
-            }while($row);
-          ?>
-        </div>
-      </div>
-  </body>
+$query = mysqli_query($conn, $SQLstring);
+if(!$query) {
+	print "query could not be executed";
+    $error = mysqli_error();
+    print "<p>" . $error . "</p>";
+    exit;
+   }
+while($rows=mysqli_fetch_array($query)){
+?>
+	<div class="center">
+	<div class="wrapper">
+	<div class="inner">
+	<div class ="card">
+	<div class ="content">
+	<?php echo "<h1>" . $rows['Company'] . "</h1>";  ?>
+	<?php echo "<h1>" .$rows['JobTitle'] . "</h1>";  ?>
+	<?php echo "<h3>" .$rows['Description'] . "</h3>";  ?>
+	<?php echo "<h3>" .$rows['Eligibility '] . "</h3>";  ?>
+	</div>
+</div>
+</div>
+<?php
+}
+mssqli_close($db);
+?> 
+</body>
 </html>
